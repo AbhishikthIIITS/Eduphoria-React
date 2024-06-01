d67a57847819a1999c560856ee79dfead29be03c
Abhishikth Boda <98799880+AbhishikthIIITS@users.noreply.github.com>
2024-06-01 12:18:11 +0530

    Update AllUsersPage.js

--- src/Pages/AllUsersPage.js
+++ src/Pages/AllUsersPage.js
@@ -3,6 +3,7 @@ import AllUsers from "../components/AdminDashboard/AllUsers";
 import { useTitle } from "../Hooks/title-hook";
 import { motion } from "framer-motion";
 import GoBackButton from "../shared/components/FrontendTools/GoBack";
+
 const AllUsersPage = () => {
   const [users, setUsers] = useState([]);
   useTitle("All Users");
